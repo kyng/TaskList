5dd49a007ed4b349164a98205dfc9714a1ec12cd
Mart Küng <mart.kung@outlook.com>
2026-07-07 19:49:26 +0300

    Class Task created

--- models.py
+++ models.py
@@ -1,11 +1,36 @@
 
 class Project:
-    pass
+    
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def move_task():
+        pass
 
 class Column:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+        task_list = []
+    
+
+    def add_task():
+        pass
+
+    def remove_task():
+        pass
+
+    def __str__(self):
+        pass
 
 class Task:
 
-    def __init__(self, id, name, description, state="planned"):
-        pass
+    def __init__(self, id: int, name: str, description: str, priority = 0):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.priority = priority
+
+    def __str__(self):
+        return f"Task {self.id} - {self.name}: {self.description}. Priority: {self.priority}"
